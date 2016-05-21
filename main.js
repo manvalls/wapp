3233fa8f18cb8d7ea199e573be8dfafd41863b84
Manuel Valls Fernández <manolo@vlrz.es>
2016-05-21 15:42:57 +0200

    Dedupe multiple wapp instances

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
 if(global.process) module.exports = require('./ser' + 'ver.js');
-else module.exports = require('./client.js');
+else module.exports = global['MxKTbNApKfpUF6R'] = global['MxKTbNApKfpUF6R'] || require('./client.js');
