3848d21d9a5c7899f5bed4886a229b88aa573aea
Manuel Valls Fernández <manolo@vlrz.es>
2016-07-20 17:13:12 +0200

    Improve the usage of babel plugins

--- utils/getBr.js
+++ utils/getBr.js
@@ -6,7 +6,6 @@ var Cb = require('y-callback/node'),
 function getBr(file,name,watch,instrument){
   var browserify = require('browserify'),
       babelify = require('babelify'),
-      es2015 = require('babel-preset-es2015'),
 
       watchify,es5,es6;
 
@@ -23,7 +22,30 @@ function getBr(file,name,watch,instrument){
   });
 
   es5.transform(babelify.configure({
-    presets: [es2015]
+    plugins: [
+      require('babel-plugin-check-es2015-constants'),
+      require('babel-plugin-transform-es2015-arrow-functions'),
+      require('babel-plugin-transform-es2015-block-scoped-functions'),
+      require('babel-plugin-transform-es2015-block-scoping'),
+      require('babel-plugin-transform-es2015-classes'),
+      require('babel-plugin-transform-es2015-computed-properties'),
+      require('babel-plugin-transform-es2015-destructuring'),
+      require('babel-plugin-transform-es2015-duplicate-keys'),
+      require('babel-plugin-transform-es2015-for-of'),
+      require('babel-plugin-transform-es2015-function-name'),
+      require('babel-plugin-transform-es2015-literals'),
+      require('babel-plugin-transform-es2015-object-super'),
+      require('babel-plugin-transform-es2015-parameters'),
+      require('babel-plugin-transform-es2015-shorthand-properties'),
+      require('babel-plugin-transform-es2015-spread'),
+      require('babel-plugin-transform-es2015-sticky-regex'),
+      require('babel-plugin-transform-es2015-template-literals'),
+      require('babel-plugin-transform-es2015-typeof-symbol'),
+      require('babel-plugin-transform-es2015-unicode-regex'),
+      require('babel-plugin-transform-exponentiation-operator'),
+      require('babel-plugin-transform-regenerator')
+    ],
+    compact: false
   }),{global: true});
 
   if(instrument){
